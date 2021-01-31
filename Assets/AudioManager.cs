beda06e75b91f0a274181e09454da32a420a9597
dalcala94 <47342981+dalcala94@users.noreply.github.com>
2021-01-31 14:18:15 -0800

    AudioManager created
    Sound List populated + more sound files

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    //List of sounds to access throughout game
+    public Sound[] sounds;
+
+    //Mixing Purposes
+    public AudioSource sfx;
+    //public AudioSource background;
+
+    //Access across all scripts (AudioManager.instance.funcName())
+    public static AudioManager instance;
+
+    //Pitch Randomization..?
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        //Each Sound Clip gets appropraitly created
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.playOnAwake = s.playOnAwake;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    //Base of Sound Class.
+    public AudioClip clip;
+    public AudioMixer group;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+    [HideInInspector]
+    public AudioSource source;
+
+    public bool loop;
+    public bool playOnAwake;
+
+}
